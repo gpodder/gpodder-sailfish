e7b9b82942cf1ffd00d165667084fbfe87b0b3ba
sfbg <sfbg@users.noreply.github.com>
2019-07-24 23:26:29 +0200

    A file for the Bulgarian translation

---
+++ translations/harbour-org.gpodder.sailfish-bg.ts
@@ -0,0 +1,466 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="bg_BG">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="47"/>
+        <source>About gPodder</source>
+        <translation>За gPodder</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="76"/>
+        <source>Thomas Perl and the gPodder Team</source>
+        <translation>Thomas Perl и gPodder отбора</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="77"/>
+        <source>License: ISC / GPLv3 or later</source>
+        <translation>Лиценз: ISC / GPLv3 или по-късен </translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="78"/>
+        <source>Website: </source>
+        <translation>Интернет страница: </translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="80"/>
+        <source>Sailfish OS artwork by Stephan Beyerle</source>
+        <translation>Sailfish OS картинки от Stephan Beyerle</translation>
+    </message>
+</context>
+<context>
+    <name>AllEpisodesPage</name>
+    <message>
+        <location filename="../qml/AllEpisodesPage.qml" line="55"/>
+        <source>Episodes: </source>
+        <translation>Епизоди: </translation>
+    </message>
+    <message>
+        <location filename="../qml/AllEpisodesPage.qml" line="71"/>
+        <source>No episodes found</source>
+        <translation>Няма намерени епизоди</translation>
+    </message>
+</context>
+<context>
+    <name>DirectoryDialog</name>
+    <message>
+        <location filename="../qml/DirectoryDialog.qml" line="51"/>
+        <source>Search term</source>
+        <translation>Текст за търсене</translation>
+    </message>
+</context>
+<context>
+    <name>DirectorySelectionDialog</name>
+    <message>
+        <location filename="../qml/DirectorySelectionDialog.qml" line="41"/>
+        <source>Select provider</source>
+        <translation>Избиране на доставчик</translation>
+    </message>
+</context>
+<context>
+    <name>EpisodeDetail</name>
+    <message>
+        <location filename="../qml/EpisodeDetail.qml" line="63"/>
+        <source>Visit website</source>
+        <translation>Интернет страница</translation>
+    </message>
+    <message>
+        <location filename="../qml/EpisodeDetail.qml" line="81"/>
+        <source>Episode details</source>
+        <translation>Детайли</translation>
+    </message>
+    <message>
+        <location filename="../qml/EpisodeDetail.qml" line="131"/>
+        <source>Chapters</source>
+        <translation>Глави</translation>
+    </message>
+    <message>
+        <location filename="../qml/EpisodeDetail.qml" line="179"/>
+        <source>Shownotes</source>
+        <translation>Описание</translation>
+    </message>
+</context>
+<context>
+    <name>EpisodeFilterDialog</name>
+    <message>
+        <location filename="../qml/EpisodeFilterDialog.qml" line="35"/>
+        <source>Filter episode list</source>
+        <translation>Филтър</translation>
+    </message>
+</context>
+<context>
+    <name>EpisodeItem</name>
+    <message>
+        <location filename="../qml/EpisodeItem.qml" line="70"/>
+        <source>Pause</source>
+        <translation>Паузиране</translation>
+    </message>
+    <message>
+        <location filename="../qml/EpisodeItem.qml" line="70"/>
+        <source>Play</source>
+        <translation>Пускане</translation>
+    </message>
+    <message>
+        <location filename="../qml/EpisodeItem.qml" line="82"/>
+        <source>Download</source>
+        <translation>Изтегляне</translation>
+    </message>
+    <message>
+        <location filename="../qml/EpisodeItem.qml" line="92"/>
+        <source>Enqueue</source>
+        <translation>В опашката</translation>
+    </message>
+    <message>
+        <location filename="../qml/EpisodeItem.qml" line="105"/>
+        <source>Delete</source>
+        <translation>Изтриване</translation>
+    </message>
+    <message>
+        <location filename="../qml/EpisodeItem.qml" line="119"/>
+        <source>Toggle New</source>
+        <translation>Като нов</translation>
+    </message>
+    <message>
+        <location filename="../qml/EpisodeItem.qml" line="125"/>
+        <source>Shownotes</source>
+        <translation>Описание</translation>
+    </message>
+</context>
+<context>
+    <name>EpisodeListFilterItem</name>
+    <message>
+        <location filename="../qml/EpisodeListFilterItem.qml" line="33"/>
+        <source>Filter: </source>
+        <translation>Филтер: </translation>
+    </message>
+</context>
+<context>
+    <name>EpisodesPage</name>
+    <message>
+        <location filename="../qml/EpisodesPage.qml" line="77"/>
+        <source>Mark episodes as old</source>
+        <translation>Отбелязване като стари</translation>
+    </message>
+    <message>
+        <location filename="../qml/EpisodesPage.qml" line="84"/>
+        <source>Enqueue episodes in player</source>
+        <translation>Добавяне към опашката</translation>
+    </message>
+    <message>
+        <location filename="../qml/EpisodesPage.qml" line="99"/>
+        <source>Unsubscribe</source>
+        <translation>Отабониране</translation>
+    </message>
+    <message>
+        <location filename="../qml/EpisodesPage.qml" line="117"/>
+        <source>No episodes found</source>
+        <translation>Няма намерени епизоди</translation>
+    </message>
+</context>
+<context>
+    <name>PlayerChaptersDialog</name>
+    <message>
+        <location filename="../qml/PlayerChaptersDialog.qml" line="37"/>
+        <source>Chapters</source>
+        <translation>Глави</translation>
+    </message>
+</context>
+<context>
+    <name>PlayerChaptersItem</name>
+    <message>
+        <location filename="../qml/PlayerChaptersItem.qml" line="33"/>
+        <source>Chapters</source>
+        <translation>Глави</translation>
+    </message>
+</context>
+<context>
+    <name>PlayerCover</name>
+    <message>
+        <location filename="../qml/PlayerCover.qml" line="44"/>
+        <source>Sleep timer: </source>
+        <translation>Отброяване до заспиване: </translation>
+    </message>
+</context>
+<context>
+    <name>PlayerPage</name>
+    <message>
+        <location filename="../qml/PlayerPage.qml" line="44"/>
+        <source>Stop sleep timer</source>
+        <translation>Спиране на отброяването</translation>
+    </message>
+    <message>
+        <location filename="../qml/PlayerPage.qml" line="44"/>
+        <source>Sleep timer</source>
+        <translation>Отброяване до заспиване</translation>
+    </message>
+    <message>
+        <location filename="../qml/PlayerPage.qml" line="55"/>
+        <source>Clear play queue</source>
+        <translation>Изчистване на опашката</translation>
+    </message>
+    <message>
+        <location filename="../qml/PlayerPage.qml" line="177"/>
+        <source>Pause</source>
+        <translation>Паузиране</translation>
+    </message>
+    <message>
+        <location filename="../qml/PlayerPage.qml" line="177"/>
+        <source>Play</source>
+        <translation>Пускане</translation>
+    </message>
+    <message>
+        <location filename="../qml/PlayerPage.qml" line="71"/>
+        <source>Now playing</source>
+        <translation>В момента върви</translation>
+    </message>
+    <message>
+        <location filename="../qml/PlayerPage.qml" line="67"/>
+        <source>Player</source>
+        <translation>Прослушвач</translation>
+    </message>
+    <message>
+        <location filename="../qml/PlayerPage.qml" line="100"/>
+        <source>Sleep timer: </source>
+        <translation>Отброяване до заспиване: </translation>
+    </message>
+    <message>
+        <location filename="../qml/PlayerPage.qml" line="158"/>
+        <source>- 1 min</source>
+        <translation>- 1 мин</translation>
+    </message>
+    <message>
+        <location filename="../qml/PlayerPage.qml" line="168"/>
+        <source>- 10 sec</source>
+        <translation>- 10 сек</translation>
+    </message>
+    <message>
+        <location filename="../qml/PlayerPage.qml" line="189"/>
+        <source>+ 10 sec</source>
+        <translation>+ 10 сек</translation>
+    </message>
+    <message>
+        <location filename="../qml/PlayerPage.qml" line="198"/>
+        <source>+ 1 min</source>
+        <translation>+ 1 мин</translation>
+    </message>
+    <message>
+        <location filename="../qml/PlayerPage.qml" line="208"/>
+        <source>Queue</source>
+        <translation>Опашка</translation>
+    </message>
+    <message>
+        <location filename="../qml/PlayerPage.qml" line="232"/>
+        <source>Remove from queue</source>
+        <translation>Премахване от опашката</translation>
+    </message>
+</context>
+<context>
+    <name>PodcastDetail</name>
+    <message>
+        <location filename="../qml/PodcastDetail.qml" line="66"/>
+        <source>Copy feed URL</source>
+        <translation>Копиране на RSS адреса</translation>
+    </message>
+    <message>
+        <location filename="../qml/PodcastDetail.qml" line="71"/>
+        <source>Visit website</source>
+        <translation>Интернет страница</translation>
+    </message>
+    <message>
+        <location filename="../qml/PodcastDetail.qml" line="89"/>
+        <source>Podcast details</source>
+        <translation>Детайли</translation>
+    </message>
+    <message>
+        <location filename="../qml/PodcastDetail.qml" line="134"/>
+        <source>Section: </source>
+        <translation>Раздел: </translation>
+    </message>
+</context>
+<context>
+    <name>PodcastItem</name>
+    <message>
+        <location filename="../qml/PodcastItem.qml" line="30"/>
+        <source>Refresh</source>
+        <translation>Обновяване</translation>
+    </message>
+    <message>
+        <location filename="../qml/PodcastItem.qml" line="39"/>
+        <source>Unsubscribe</source>
+        <translation>Отабониране</translation>
+    </message>
+    <message>
+        <location filename="../qml/PodcastItem.qml" line="44"/>
+        <source>Unsubscribing</source>
+        <translation>Абонаметът се прекратява</translation>
+    </message>
+    <message>
+        <location filename="../qml/PodcastItem.qml" line="51"/>
+        <location filename="../qml/PodcastItem.qml" line="58"/>
+        <source>Rename</source>
+        <translation>Преименуване</translation>
+    </message>
+    <message>
+        <location filename="../qml/PodcastItem.qml" line="57"/>
+        <source>Rename podcast</source>
+        <translation>Преименуване</translation>
+    </message>
+    <message>
+        <location filename="../qml/PodcastItem.qml" line="59"/>
+        <source>Podcast name</source>
+        <translation>Име</translation>
+    </message>
+    <message>
+        <location filename="../qml/PodcastItem.qml" line="69"/>
+        <location filename="../qml/PodcastItem.qml" line="75"/>
+        <source>Change section</source>
+        <translation>Промяна на раздела</translation>
+    </message>
+    <message>
+        <location filename="../qml/PodcastItem.qml" line="76"/>
+        <source>Move</source>
+        <translation>Преместване</translation>
+    </message>
+    <message>
+        <location filename="../qml/PodcastItem.qml" line="77"/>
+        <source>Section</source>
+        <translation>Раздел</translation>
+    </message>
+    <message>
+        <location filename="../qml/PodcastItem.qml" line="87"/>
+        <source>Podcast detail</source>
+        <translation>Детайл</translation>
+    </message>
+</context>
+<context>
+    <name>PodcastsCover</name>
+    <message>
+        <location filename="../qml/PodcastsCover.qml" line="35"/>
+        <source>podcasts</source>
+        <translation>подкаста</translation>
+    </message>
+    <message>
+        <location filename="../qml/PodcastsCover.qml" line="36"/>
+        <source>episodes</source>
+        <translation>епизода</translation>
+    </message>
+    <message>
+        <location filename="../qml/PodcastsCover.qml" line="37"/>
+        <source>new episodes</source>
+        <translation>нови епизода</translation>
+    </message>
+    <message>
+        <location filename="../qml/PodcastsCover.qml" line="38"/>
+        <source>downloaded</source>
+        <translation>изтеглени</translation>
+    </message>
+</context>
+<context>
+    <name>PodcastsPage</name>
+    <message>
+        <location filename="../qml/PodcastsPage.qml" line="41"/>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="../qml/PodcastsPage.qml" line="46"/>
+        <source>Filter episodes</source>
+        <translation>Филтриране на епизодите</translation>
+    </message>
+    <message>
+        <location filename="../qml/PodcastsPage.qml" line="51"/>
+        <source>Checking for new episodes...</source>
+        <translation>Проверява се за нови...</translation>
+    </message>
+    <message>
+        <location filename="../qml/PodcastsPage.qml" line="51"/>
+        <source>Check for new episodes</source>
+        <translation>Проверяване за нови епизоди</translation>
+    </message>
+    <message>
+        <location filename="../qml/PodcastsPage.qml" line="59"/>
+        <source>Add new podcast</source>
+        <translation>Добавяне на нов подкаст</translation>
+    </message>
+    <message>
+        <location filename="../qml/PodcastsPage.qml" line="64"/>
+        <source>Discover new podcasts</source>
+        <translation>Намиране на нови подкасти</translation>
+    </message>
+    <message>
+        <location filename="../qml/PodcastsPage.qml" line="70"/>
+        <source>Subscriptions</source>
+        <translation>Абонаменти</translation>
+    </message>
+    <message>
+        <location filename="../qml/PodcastsPage.qml" line="87"/>
+        <source>No subscriptions</source>
+        <translation>Няма абонаменти</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="53"/>
+        <source>About</source>
+        <translation>Относно</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="63"/>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="67"/>
+        <source>YouTube</source>
+        <translation>YouTube</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="73"/>
+        <source>API Key (v3)</source>
+        <translation>API ключ (v3)</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="82"/>
+        <source>Limits</source>
+        <translation>Ограничения</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="88"/>
+        <source>Maximum episodes per feed</source>
+        <translation>Максимален брой епизоди за RSS емисия</translation>
+    </message>
+</context>
+<context>
+    <name>SleepTimerDialog</name>
+    <message>
+        <location filename="../qml/SleepTimerDialog.qml" line="34"/>
+        <source>Sleep timer</source>
+        <translation>Отброяване до заспиване</translation>
+    </message>
+    <message>
+        <location filename="../qml/SleepTimerDialog.qml" line="57"/>
+        <source> minutes</source>
+        <translation> минути</translation>
+    </message>
+</context>
+<context>
+    <name>Subscribe</name>
+    <message>
+        <location filename="../qml/Subscribe.qml" line="35"/>
+        <source>Add subscription</source>
+        <translation>Добавяне на абонамент</translation>
+    </message>
+    <message>
+        <location filename="../qml/Subscribe.qml" line="36"/>
+        <source>Subscribe</source>
+        <translation>Абониране</translation>
+    </message>
+    <message>
+        <location filename="../qml/Subscribe.qml" line="42"/>
+        <source>Feed URL</source>
+        <translation>Адрес на RSS емисията</translation>
+    </message>
+</context>
+</TS>
